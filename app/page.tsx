7733bf9a9156ca0247d0915bffb9e424c3c6f46b
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2023-07-07 20:17:07 -0500

    Agregar momentode carga a la pagina y volver a inicializar con el boton iniciar pero con un pequeño error

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,46 @@
 // @ts-nocheck
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from '../src/board';
 import clasecarta from '@/src/cartaclase';
 import Mano from '@/src/mano';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-
 const Home = () => {
-  const [vectorcartas, setVectorCartas] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const [vectorcartas, setVectorCartas] = useState<clasecarta[]>([]);
   const iniciar = () => {
-    const car1 = clasecarta.conseguircarta();
-    const car2 = clasecarta.conseguircarta();
-    const car3 = clasecarta.conseguircarta();
-    const car4 = clasecarta.conseguircarta();
-    const car5 = clasecarta.conseguircarta();
-    const nuevoVectorCartas = [car1, car2, car3, car4, car5];
-    setVectorCartas(nuevoVectorCartas);
+    setLoading(true);
+    setTimeout(() => {
+      const car1 = clasecarta.conseguircarta();
+      const car2 = clasecarta.conseguircarta();
+      const car3 = clasecarta.conseguircarta();
+      const car4 = clasecarta.conseguircarta();
+      const car5 = clasecarta.conseguircarta();
+      const nuevoVectorCartas = [car1, car2, car3, car4, car5];
+      setVectorCartas(nuevoVectorCartas);
+      setLoading(false);
+    }, 2000); // Simulando una carga de 2 segundos
   };
+
+  useEffect(() => {
+    iniciar();
+  }, []); // Iniciar carga al montar el componente
 
   return (
     <div>
       <h1>¡Bienvenido al tablero de cartas!</h1>
-      <button onClick={iniciar}>Iniciar</button>
-      <DndProvider backend={HTML5Backend}>
-        <Board />
-        <Mano cartas={vectorcartas} />
-      </DndProvider>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        <>
+          <button onClick={iniciar}>Iniciar</button>
+          <DndProvider backend={HTML5Backend}>
+            <Board />
+            <Mano cartas={vectorcartas} />
+          </DndProvider>
+        </>
+      )}
     </div>
   );
 };
